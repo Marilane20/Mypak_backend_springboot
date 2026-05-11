3204107574b313b1cd11457e926467abb1a2e4b7
Marilane20 <marilanekamga@gmail.com>
2026-05-11 17:37:17 +0100

    initialisation du projet

---
+++ MyPak/src/main/java/com/houseproject/MyPak/MyPakApplication.java
@@ -0,0 +1,13 @@
+package com.houseproject.MyPak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPakApplication.class, args);
+	}
+
+}
---
+++ MyPak/src/test/java/com/houseproject/MyPak/MyPakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.houseproject.MyPak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
